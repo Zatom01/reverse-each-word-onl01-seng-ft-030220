c32160d8912a4d03dad89c503ebf6afb442cd8be
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-04 19:35:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,21 @@
-def reverse_each_word(word)
-  size=word.size
-  counter=size-1
-  word.each do |letter|
-    return word[counter]
-    counter-=1
+
+def reverse_each_word(array)
+  new_array=[]
+  final_array=[]
+
+  first splitting each word,and putting them into new array
+  array.split(" ").each do|element|
+    new_array<<element
   end
+
+  iterating over every word in new array and adding in reverse order
+  new_array.each do|word|
+    final_array<<(word.reverse())
+  end
+
+  final_array.join(" ")
+
+  
 end
+
+reverse_each_word("I have one, two and five dollars")
