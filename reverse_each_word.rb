1ceeadd3d1ccdf5645c1efaf9b1bda70aa2b092e
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-04 19:40:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ def reverse_each_word(array)
   final_array=[]
 
   # first splitting each word,and putting them into new array
-  array.split(" ").each do|element|
+  array.split(" ").collect do|element|
     new_array<<element
   end
 
@@ -18,22 +18,3 @@ def reverse_each_word(array)
   
 end
 
-
-def reverse_each_word(array)
-  new_array=[]
-  final_array=[]
-
-  # first splitting each word,and putting them into new array
-  array.split(" ").collect do|element|
-    new_array<<element
-  end
-
-  # iterating over every word in new array and adding in reverse order
-  new_array.collect do|word|
-    final_array<<(word.reverse())
-  end
-
-  final_array.join(" ")
-
-  
-end
